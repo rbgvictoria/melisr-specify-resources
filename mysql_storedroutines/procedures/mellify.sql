87adfa07bf9bda22795092ad9ac828a0a4a44fb5
Niels Klazenga <NielsKlazenga@rbg.vic.gov.au>
2016-10-11 17:26:49 +1100

    Procedure to bring records from the staging collection to the MEL collection once they have been assigned a catalogue number.

---
+++ mysql_storedroutines/procedures/mellify.sql
@@ -0,0 +1,40 @@
+/*
+*
+* Mellify procedure
+*
+* Moves records from the staging collection to the National Herbarium of Victoria
+* collection and correctly formats the catalogue number and barcode. [2016-10-11]
+*
+*/
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS mellify $$
+CREATE PROCEDURE mellify ()
+BEGIN
+    START TRANSACTION;
+    SET @DISABLE_TRIGGER=1;
+    UPDATE collectionobject co
+    LEFT JOIN collectionobjectattribute coa ON co.CollectionObjectAttributeID=coa.CollectionObjectAttributeID
+    LEFT JOIN determination d ON co.CollectionObjectID=d.CollectionObjectID
+    LEFT JOIN preparation p ON co.CollectionObjectID=p.CollectionObjectID
+    LEFT JOIN otheridentifier oi ON co.CollectionObjectID=oi.CollectionObjectID
+    SET co.AltCatalogNumber=CAST(SUBSTR(co.CatalogNumber, 5) AS UNSIGNED),
+      co.Modifier='A',
+      co.Name=CONCAT('MEL ', CAST(SUBSTR(co.CatalogNumber, 5) AS UNSIGNED)),
+      co.CatalogNumber=CONCAT(SUBSTR(co.CatalogNumber, 5), 'A'),
+      co.CollectionID=4,
+      co.CollectionMemberID=4,
+      coa.CollectionMemberID=4,
+      d.CollectionMemberID=4,
+      p.CollectionMemberID=4,
+      oi.CollectionMemberID=4,
+      co.TimestampModified=NOW()
+    WHERE co.CollectionID=131072 AND co.CatalogNumber IS NOT NULL;
+    SET @DISABLE_TRIGGER=NULL;
+    COMMIT;
+END $$
+
+DELIMITER ;
+
+
+
