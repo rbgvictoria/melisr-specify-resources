d844d26f677c72d0e400ff318bb6692945b08710
Niels Klazenga <NielsKlazenga@rbg.vic.gov.au>
2016-10-25 16:06:31 +1100

    Fixed procedure name in update_dwc_identification_qualifier.sql

--- mysql_storedroutines/procedures/update_dwc_identification_qualifier.sql
+++ mysql_storedroutines/procedures/update_dwc_identification_qualifier.sql
@@ -1,7 +1,7 @@
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS update_dwc_type_status $$
-CREATE PROCEDURE update_dwc_type_status ()
+DROP PROCEDURE IF EXISTS update_dwc_identification_qualifier $$
+CREATE PROCEDURE update_dwc_identification_qualifier ()
 BEGIN
     UPDATE determination
     SET Text1=dwc_identification_qualifier(Qualifier, VarQualifier, TaxonID)
