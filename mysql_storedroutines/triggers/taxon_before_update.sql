1d56dca0b5be7bb086f2f9fb6f57596b4d52566e
Niels Klazenga <Niels.Klazenga@rbg.vic.gov.au>
2023-04-26 12:45:17 +1000

    update taxon_before_update trigger

--- mysql_storedroutines/triggers/taxon_before_update.sql
+++ mysql_storedroutines/triggers/taxon_before_update.sql
@@ -10,20 +10,21 @@ DELIMITER $$
 CREATE TRIGGER taxon_before_update BEFORE UPDATE ON taxon
 FOR EACH ROW
     BEGIN
+	      DECLARE var_fullname VARCHAR(256);
+	    
         IF isnull(@DISABLE_TRIGGER) THEN 
-          IF NEW.FullName != OLD.FullName OR NEW.Author != OLD.Author THEN
-            IF !isnull(NEW.EsaStatus) AND NEW.EsaStatus!='' THEN
-                SET var_fullname = IF(NEW.FullName LIKE '%[%', SUBSTRING(NEW.FullName, 1, LOCATE('[', NEW.FullName)-2), NEW.FullName);
-                SET NEW.FullName = CONCAT(var_fullname, ' [', NEW.EsaStatus, ']');
+            IF NEW.FullName != OLD.FullName OR NEW.Author != OLD.Author THEN
+                IF !isnull(NEW.EsaStatus) AND NEW.EsaStatus!='' THEN
+                    SET var_fullname = IF(NEW.FullName LIKE '%[%', SUBSTRING(NEW.FullName, 1, LOCATE('[', NEW.FullName)-2), NEW.FullName);
+                    SET NEW.FullName = CONCAT(var_fullname, ' [', NEW.EsaStatus, ']');
+                END IF;
             END IF;
 
-          END IF;
-
-          IF NEW.TaxonTreeDefItemID=12 
-              AND ((NEW.Text3 IS NOT NULL AND (NEW.Text3<>OLD.Text3 OR OLD.Text3 IS NULL)) 
-              OR  (NEW.Text4 IS NOT NULL AND (NEW.Text4<>OLD.Text4 OR OLD.Text4 IS NULL))) THEN
-            SET NEW.YesNo1=true;
-          END IF;
+            IF NEW.TaxonTreeDefItemID=12 
+                AND ((NEW.Text3 IS NOT NULL AND (NEW.Text3<>OLD.Text3 OR OLD.Text3 IS NULL)) 
+                OR  (NEW.Text4 IS NOT NULL AND (NEW.Text4<>OLD.Text4 OR OLD.Text4 IS NULL))) THEN
+                SET NEW.YesNo1=true;
+            END IF;
         END IF;    
     END $$
 
