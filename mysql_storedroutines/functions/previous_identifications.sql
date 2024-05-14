2e8012a7fbecbc0b25a249299a973982931dde8d
Niels Klazenga <niels.klazenga@rbg.vic.gov.au>
2024-05-14 20:47:42 +1000

    add identification qualifiers to previousIdentifications

--- mysql_storedroutines/functions/previous_identifications.sql
+++ mysql_storedroutines/functions/previous_identifications.sql
@@ -6,11 +6,11 @@ DROP function IF EXISTS `previous_identifications`;
 
 DELIMITER $$
 USE `melisr`$$
-CREATE DEFINER=`admin`@`%` FUNCTION `previous_identifications`(in_collection_object_id INTEGER) RETURNS text CHARSET utf8
+CREATE DEFINER=`admin`@`%` FUNCTION `melisr`.`previous_identifications`(in_collection_object_id INTEGER) RETURNS text CHARSET utf8 COLLATE utf8_general_ci
 BEGIN
 	DECLARE out_previous_identifications TEXT;
 	select 
-	  group_concat(identification_string(concat_ws(', ', concat_ws(' ', if(t.FullName LIKE '% [%' , substring(t.FullName, 1, LOCATE(' [', t.FullName)-1), t.FullName), t.author), t.EsaStatus), 
+	  group_concat(identification_string(concat_ws(', ', concat_ws(' ', if(d.Qualifier is not null, fn_verbatim_identification(d.DeterminationID), if(t.FullName LIKE '% [%' , substring(t.FullName, 1, LOCATE(' [', t.FullName)-1), t.FullName)), t.author), t.EsaStatus), 
 		concat_ws(' ', a.LastName, a.FirstName), dateWithPrecision(d.DeterminedDate, d.DeterminedDatePrecision), d.Remarks) SEPARATOR ' | ') as previousIdentifications
 	into out_previous_identifications
 	from determination d
